c91b8d3f9a70c40844d6d712419cff2e7ae94e8e
James Harris <stoffle@gmail.com>
2013-01-19 14:07:38 +1000

    Added test for Console\Application

---
+++ test/suite/Icecave/Woodhouse/Console/ApplicationTest.php
@@ -0,0 +1,21 @@
+<?php
+namespace Icecave\Woodhouse\Console;
+
+use Phake;
+use PHPUnit_Framework_TestCase;
+
+class ApplicationTest extends PHPUnit_Framework_TestCase
+{
+    public function setUp()
+    {
+        $this->_application = Phake::partialMock(__NAMESPACE__ . '\Application');
+    }
+
+    public function testConstructor()
+    {
+        $this->assertSame('Woodhouse', $this->_application->getName());
+        $this->assertSame('DEV', $this->_application->getVersion());
+
+        Phake::verify($this->_application)->add($this->isInstanceOf(__NAMESPACE__ . '\Command\PublishCommand'));
+    }
+}
